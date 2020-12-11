4534828f9e5cf0aa445bb17b978f73c8ecd644a1
Edgar Lopez <elopez@bluepeople.com>
2020-12-11 12:04:20 -0600

    se agrego la funcion del boton y opacidad

---
+++ index.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "500px";
+  document.getElementById("myDiv").style.opacity = "0.5";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("myDiv").style.opacity = "1";
+}
